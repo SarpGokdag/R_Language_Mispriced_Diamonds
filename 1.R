7fb9291fb2b2820cb37a69280274721b3adc081a
Sarp Gökdağ <sarpgo@icloud.com>
2017-07-06 11:54:03 +0300

    Delete 1.R

--- 1.R
+++
@@ -1,9 +0,0 @@
-mydata <- read.csv(file.choose())
-install.packages("ggplot2")
-
-ggplot(data=mydata[mydata$Karat<2.5,], aes(x=Karat, y=Fiyat , colour=Parlaklık)) + 
-  geom_point(alpha=1) +
-  geom_smooth()
-
-
-
